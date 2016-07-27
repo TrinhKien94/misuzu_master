6316579ecc57ec914af186715e3ebd1a3ccd4505
tranduytung <tranduytung1994@gmail.com>
2016-07-27 10:51:54 +0700

    Revert "fix main controller"

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,8 @@
 class MainController < ApplicationController
-  before_action :require_user!
-
   def index
-    @kairanCount = Kairanshosai.where(対象者: session[:user], 確認: false).count
-    shain = Shainmaster.find_by id: session[:user]
-    shain.update_attributes 回覧件数: @kairanCount
+     @kairanCount = Kairanshosai.where(対象者: session[:user], 確認: false).count
+    shain = Shainmaster.find(session[:user])
+    shain.回覧件数 = @kairanCount
+    shain.save
   end
 end
